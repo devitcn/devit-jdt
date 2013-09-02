ee2f4ac9479a68dc98e288f8118aaf164b44df6d
lxbzmy <lxbzmy@gmail.com>
2013-09-02 15:22:32 +0800

    delete sysout.

--- src/cn/devit/jdt/Activator.java
+++ src/cn/devit/jdt/Activator.java
@@ -29,7 +29,6 @@ public class Activator extends AbstractUIPlugin {
      */
     public void start(BundleContext context) throws Exception {
         super.start(context);
-        System.out.println("start cn.devit.jdt");
         plugin = this;
     }
 
