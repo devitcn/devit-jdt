a94099a661a871319e95670573c0b0b71b30dd37
lxb <lxbzmy@gmail.com>
2018-08-03 23:09:57 +0800

    借用get方法的返回类型注释

--- cn.devit.jdt.hover/src/cn/devit/jdt/hover/JavadocHover.java
+++ cn.devit.jdt.hover/src/cn/devit/jdt/hover/JavadocHover.java
@@ -7,6 +7,7 @@ import org.eclipse.jdt.core.IMember;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.Signature;
 import org.eclipse.jdt.internal.ui.text.java.hover.JavadocBrowserInformationControlInput;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
@@ -44,7 +45,6 @@ public class JavadocHover
             if (member instanceof IMethod) {
                 try {
                     if (((IMethod) member).getJavadocRange() != null) {
-                        System.out.println("has doc skip.");
                     } else {
                         String name = member.getElementName();
                         boolean isGetter = name.startsWith("get") //$NON-NLS-1$
@@ -63,6 +63,13 @@ public class JavadocHover
                             //如果有field
                             //并且field字段有javadoc
                             if(field==null || field.getJavadocRange()==null) {
+                                /*
+                                 * 使用返回值类型的注释
+                                 */
+                                if(isGetter) {
+                                    return useReturnTypeDoc(hoverRegion, member,
+                                            type);
+                                }
                                 return null;
                             }
                             elements = new IJavaElement[] { field };
@@ -85,9 +92,84 @@ public class JavadocHover
         return null;
     }
 
+    Object useReturnTypeDoc(IRegion hoverRegion, IJavaElement member,
+            IType type) throws JavaModelException {
+        String returnTypeQName = ((IMethod) member).getReturnType();
+        Signature.toString(returnTypeQName);
+        
+        String elementTypeSignature = Signature.getElementType(returnTypeQName);
+        if (elementTypeSignature.length() == 1) {
+            // JDK内置类，不管了
+            return null;
+        }
+        String elementTypeName = Signature.toString(elementTypeSignature);
+        String[][] resolvedElementTypeNames = type.resolveType(elementTypeName);
+        if (resolvedElementTypeNames == null){
+            return null;
+        }
+        String[] types = resolvedElementTypeNames[0];
+        String resolvedElementTypeName = Signature.toQualifiedName(types);
+        try {
+            IType returnType = type.getJavaProject().findType(resolvedElementTypeName);
+            if(returnType.getJavadocRange()==null) {
+                return null;
+            }
+            JavadocBrowserInformationControlInput e = getHoverInfo(new IJavaElement[] { returnType}, returnType.getTypeRoot(), hoverRegion,
+                    null);
+            return e;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
     public static String firstToLower(String propertyName) {
         char[] c = propertyName.toCharArray();
         c[0] = Character.toLowerCase(c[0]);
         return String.valueOf(c);
     }
 }
+/*
+ * private static String resolveTypeSignature(IMethod method, String typeSignature) throws JavaModelException {
+        int count = Signature.getArrayCount(typeSignature);
+        String elementTypeSignature = Signature.getElementType(typeSignature);
+        if (elementTypeSignature.length() == 1) {
+            // no need to resolve primitive types
+            return typeSignature;
+        }
+        String elementTypeName = Signature.toString(elementTypeSignature);
+        IType type = method.getDeclaringType();
+        String[][] resolvedElementTypeNames = type.resolveType(elementTypeName);
+        if (resolvedElementTypeNames == null || resolvedElementTypeNames.length != 1) {
+            // check if type parameter
+            ITypeParameter typeParameter = method.getTypeParameter(elementTypeName);
+            if (!typeParameter.exists()) {
+                typeParameter = type.getTypeParameter(elementTypeName);
+            }
+            if (typeParameter.exists()) {
+                String[] bounds = typeParameter.getBounds();
+                if (bounds.length == 0) {
+                    return "Ljava/lang/Object;"; //$NON-NLS-1$
+                }
+                String bound = Signature.createTypeSignature(bounds[0], false);
+                return Signature.createArraySignature(resolveTypeSignature(method, bound), count);
+            }
+            // the type name cannot be resolved
+            return null;
+        }
+
+        String[] types = resolvedElementTypeNames[0];
+        types[1] = types[1].replace('.', '$');
+
+        String resolvedElementTypeName = Signature.toQualifiedName(types);
+        String resolvedElementTypeSignature = "";
+        if(types[0].equals("")) {
+            resolvedElementTypeName = resolvedElementTypeName.substring(1);
+            resolvedElementTypeSignature = Signature.createTypeSignature(resolvedElementTypeName, true);
+        }
+        else {
+            resolvedElementTypeSignature = Signature.createTypeSignature(resolvedElementTypeName, true).replace('.', '/');
+        }
+
+        return Signature.createArraySignature(resolvedElementTypeSignature, count);
+    }
+*/
